beeddf039d73d8f207cb0d73ec241e725f5fbc20
Jeffrey Pursell <jpursell@gmail.com>
2025-11-30 13:44:35 -0500

    Feat: Populate day04.rs with solution from old repo and update Cargo.toml

--- src/solutions/year_2024/solutions.rs
+++ src/solutions/year_2024/solutions.rs
@@ -4,6 +4,7 @@ pub fn get_solutions() -> std::collections::HashMap<u8, Box<dyn crate::AocSoluti
         std::collections::HashMap::new();
     map.insert(3, Box::new(super::day03::Day03));
     map.insert(1, Box::new(super::day01::Day01));
+    map.insert(4, Box::new(super::day04::Day04));
     map.insert(2, Box::new(super::day02::Day02));
     map
 }
