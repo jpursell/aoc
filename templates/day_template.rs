d05ce38e3439a2633d966501b718c4e92d4e5491
Jeffrey Pursell <jpursell@gmail.com>
2025-11-30 12:57:22 -0500

    Add unit tests to template

--- templates/day_template.rs
+++ templates/day_template.rs
@@ -11,3 +11,32 @@ impl AocSolution for Day{DAY} {
         "Not implemented".to_string()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = r"";
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(Day{DAY}.part1(EXAMPLE), "REPLACE_WITH_PART1_EXAMPLE_RESULT");
+    }
+
+    #[test]
+    fn test_part1_full() {
+        let input = crate::get_input_for_day(2024, {DAY}).expect("Failed to get input");
+        assert_eq!(Day{DAY}.part1(&input), "REPLACE_WITH_PART1_FULL_RESULT");
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(Day{DAY}.part2(EXAMPLE), "REPLACE_WITH_PART2_EXAMPLE_RESULT");
+    }
+
+    #[test]
+    fn test_part2_full() {
+        let input = crate::get_input_for_day(2024, {DAY}).expect("Failed to get input");
+        assert_eq!(Day{DAY}.part2(&input), "REPLACE_WITH_PART2_FULL_RESULT");
+    }
+}
