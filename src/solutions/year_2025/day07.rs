4175917606578ddfb9f6f59e6726e0f170071eee
Jeffrey Pursell <jpursell@gmail.com>
2025-12-07 09:40:05 -0500

    Finish 2025 7 part 2

--- src/solutions/year_2025/day07.rs
+++ src/solutions/year_2025/day07.rs
@@ -5,8 +5,7 @@ use crate::AocSolution;
 enum Token {
     Space,
     Splitter,
-    Start,
-    Tachyon,
+    Tachyon(u64),
 }
 
 fn parse(input: &str) -> Array2<Token> {
@@ -23,44 +22,47 @@ fn parse(input: &str) -> Array2<Token> {
         .map(|c| match c {
             '.' => Token::Space,
             '^' => Token::Splitter,
-            'S' => Token::Start,
+            'S' => Token::Tachyon(1),
             _ => panic!("Unexpected token"),
         })
         .collect();
     Array2::from_shape_vec((nrows, ncols), tokens).expect("Could not form 2d array")
 }
 
-fn simulate_beams(mut map: Array2<Token>) -> u64 {
+fn simulate_beams(mut map: Array2<Token>) -> (u64, u64) {
     let nrows = map.shape()[0];
     let ncols = map.shape()[1];
     let mut splits = 0;
     for irow in 0..nrows {
         for icol in 0..ncols {
-            if matches!(map[[irow, icol]], Token::Start | Token::Tachyon) {
-                splits += set_tachyon(map.view_mut(), (irow + 1, icol));
+            if let Token::Tachyon(n) = map[[irow, icol]] {
+                splits += set_tachyon(map.view_mut(), (irow + 1, icol), n);
             }
         }
     }
-    splits
+    let npaths = map
+        .row(nrows - 1)
+        .iter()
+        .map(|t| if let Token::Tachyon(n) = t { n } else { &0 })
+        .sum();
+    (splits, npaths)
 }
 
-fn count_particle_paths(map: Array2<Token>) -> u64 {
-    0
-}
-
-fn set_tachyon(mut map: ArrayViewMut2<Token>, pos: (usize, usize)) -> u64 {
+fn set_tachyon(mut map: ArrayViewMut2<Token>, pos: (usize, usize), n: u64) -> u64 {
     match map.get(pos) {
         Some(Token::Space) => {
-            *(map.get_mut(pos).unwrap()) = Token::Tachyon;
+            *(map.get_mut(pos).unwrap()) = Token::Tachyon(n);
             0
         }
         Some(Token::Splitter) => {
-            set_tachyon(map.view_mut(), (pos.0, pos.1 - 1))
-                + set_tachyon(map.view_mut(), (pos.0, pos.1 + 1))
+            set_tachyon(map.view_mut(), (pos.0, pos.1 - 1), n)
+                + set_tachyon(map.view_mut(), (pos.0, pos.1 + 1), n)
                 + 1
         }
-        Some(Token::Start) => panic!("Unexpected start"),
-        Some(Token::Tachyon) => 0,
+        Some(Token::Tachyon(x)) => {
+            *(map.get_mut(pos).unwrap()) = Token::Tachyon(n + x);
+            0
+        }
         None => 0,
     }
 }
@@ -69,11 +71,11 @@ pub struct Day07;
 
 impl AocSolution for Day07 {
     fn part1(&self, input: &str) -> String {
-        simulate_beams(parse(input)).to_string()
+        simulate_beams(parse(input)).0.to_string()
     }
 
     fn part2(&self, input: &str) -> String {
-        count_particle_paths(parse(input)).to_string()
+        simulate_beams(parse(input)).1.to_string()
     }
 }
 
@@ -117,6 +119,6 @@ mod tests {
     #[test]
     fn test_part2_full() {
         let input = crate::get_input_for_day(2025, 7).expect("Failed to get input");
-        assert_eq!(Day07.part2(&input), "REPLACE_WITH_PART2_FULL_RESULT");
+        assert_eq!(Day07.part2(&input), "47274292756692");
     }
 }
