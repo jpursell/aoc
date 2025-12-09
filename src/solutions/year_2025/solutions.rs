e6d2d546cd7031a4c218990940c56f7a90437d3d
Jeffrey Pursell <jpursell@gmail.com>
2025-12-09 03:58:53 -0500

    Scaffold 2025 09

--- src/solutions/year_2025/solutions.rs
+++ src/solutions/year_2025/solutions.rs
@@ -10,5 +10,6 @@ pub fn get_solutions() -> std::collections::HashMap<u8, Box<dyn crate::AocSoluti
     map.insert(6, Box::new(super::day06::Day06));
     map.insert(7, Box::new(super::day07::Day07));
     map.insert(8, Box::new(super::day08::Day08));
+    map.insert(9, Box::new(super::day09::Day09));
     map
 }
