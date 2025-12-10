80b5e34bfcc836adc60d6863498e42a588cbf44f
Jeffrey Pursell <jpursell@gmail.com>
2025-12-10 10:13:25 -0500

    Get part 2 example, full too slow

--- src/solutions/year_2025/day10.rs
+++ src/solutions/year_2025/day10.rs
@@ -2,18 +2,24 @@ use std::{collections::HashSet, str::FromStr};
 
 use crate::AocSolution;
 
-type State = Vec<bool>;
+type Indicator = Vec<bool>;
 type Button = Vec<usize>;
+type Joltage = Vec<u16>;
 
 #[derive(Debug)]
 struct Machine {
-    required: State,
+    required: Indicator,
     buttons: Vec<Button>,
+    joltage: Joltage,
 }
 
-fn button_press(mut state: State, button: &Button) -> State {
+fn button_press_indicator(mut state: Indicator, button: &Button) -> Indicator {
     button.iter().for_each(|&i| state[i] = !state[i]);
     state
+}
+fn button_press_joltage(mut joltage: Joltage, button: &Button) -> Joltage {
+    button.iter().for_each(|&i| joltage[i] += 1);
+    joltage
 }
 
 impl FromStr for Machine {
@@ -27,7 +33,11 @@ impl FromStr for Machine {
         assert_eq!(required.next_back().unwrap(), ']');
         let required = required.map(|c| c == '#').collect();
 
-        let _ = parts.next_back().unwrap();
+        let mut joltage = parts.next_back().unwrap().chars();
+        assert_eq!(joltage.next().unwrap(), '{');
+        assert_eq!(joltage.next_back().unwrap(), '}');
+        let joltage: String = joltage.collect();
+        let joltage = joltage.split(",").map(|s| s.parse().unwrap()).collect();
 
         let buttons = parts
             .map(|s| {
@@ -38,7 +48,11 @@ impl FromStr for Machine {
                 trimmed.split(",").map(|s| s.parse().unwrap()).collect()
             })
             .collect();
-        Ok(Machine { required, buttons })
+        Ok(Machine {
+            required,
+            buttons,
+            joltage,
+        })
     }
 }
 
@@ -46,11 +60,11 @@ fn parse(input: &str) -> Vec<Machine> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-fn find_fewest_buttons(machine: &Machine) -> u64 {
-    let mut states_a: HashSet<State> = HashSet::new();
-    let mut states_b: HashSet<State> = HashSet::new();
+fn find_fewest_buttons_indicator_lights(machine: &Machine) -> u64 {
+    let mut states_a: HashSet<Indicator> = HashSet::new();
+    let mut states_b: HashSet<Indicator> = HashSet::new();
     let mut presses: u64 = 0;
-    let start: State = machine.required.iter().map(|_| false).collect();
+    let start: Indicator = machine.required.iter().map(|_| false).collect();
     states_a.insert(start);
     loop {
         presses += 1;
@@ -61,11 +75,44 @@ fn find_fewest_buttons(machine: &Machine) -> u64 {
         };
         for button in machine.buttons.iter() {
             for state in current.iter() {
-                let new_state = button_press(state.clone(), button);
+                let new_state = button_press_indicator(state.clone(), button);
                 if new_state == machine.required {
                     return presses;
                 }
                 next.insert(new_state);
+            }
+        }
+        current.clear();
+    }
+}
+
+fn find_fewest_buttons_joltage(machine: &Machine) -> u64 {
+    let mut joltages_a: HashSet<Joltage> = HashSet::new();
+    let mut joltages_b: HashSet<Joltage> = HashSet::new();
+    let mut presses: u64 = 0;
+    let start: Joltage = machine.required.iter().map(|_| 0).collect();
+    joltages_a.insert(start);
+    loop {
+        presses += 1;
+        let (current, next) = if presses.is_multiple_of(2) {
+            (&mut joltages_b, &mut joltages_a)
+        } else {
+            (&mut joltages_a, &mut joltages_b)
+        };
+        for button in machine.buttons.iter() {
+            for joltage in current.iter() {
+                let new_joltage = button_press_joltage(joltage.clone(), button);
+                if new_joltage == machine.joltage {
+                    return presses;
+                }
+                if new_joltage
+                    .iter()
+                    .zip(machine.joltage.iter())
+                    .any(|(new, desired)| new > desired)
+                {
+                    continue;
+                }
+                next.insert(new_joltage);
             }
         }
         current.clear();
@@ -78,13 +125,17 @@ impl AocSolution for Day10 {
     fn part1(&self, input: &str) -> String {
         parse(input)
             .iter()
-            .map(find_fewest_buttons)
+            .map(find_fewest_buttons_indicator_lights)
             .sum::<u64>()
             .to_string()
     }
 
-    fn part2(&self, _input: &str) -> String {
-        "Not implemented".to_string()
+    fn part2(&self, input: &str) -> String {
+        parse(input)
+            .iter()
+            .map(find_fewest_buttons_joltage)
+            .sum::<u64>()
+            .to_string()
     }
 }
 
@@ -98,7 +149,7 @@ mod tests {
 
     #[test]
     fn test_hash_set_of_vecs() {
-        let set: HashSet<State> = HashSet::from([vec![false, true]]);
+        let set: HashSet<Indicator> = HashSet::from([vec![false, true]]);
         assert!(set.contains(&vec![false, true]));
         assert!(!set.contains(&vec![false, false]));
     }
@@ -111,12 +162,12 @@ mod tests {
     #[test]
     fn test_part1_full() {
         let input = crate::get_input_for_day(2025, 10).expect("Failed to get input");
-        assert_eq!(Day10.part1(&input), "REPLACE_WITH_PART1_FULL_RESULT");
+        assert_eq!(Day10.part1(&input), "419");
     }
 
     #[test]
     fn test_part2_example() {
-        assert_eq!(Day10.part2(EXAMPLE), "REPLACE_WITH_PART2_EXAMPLE_RESULT");
+        assert_eq!(Day10.part2(EXAMPLE), "33");
     }
 
     #[test]
