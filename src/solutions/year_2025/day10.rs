2cf7367b5e4b03864846f919b0d39cdd9c0a5b63
Jeffrey Pursell <jpursell@gmail.com>
2026-09-15 08:53:38 -0400

    Work on 2025-10
    part 2 examplt now passes!
    part 2 full is just too slow though.

--- src/solutions/year_2025/day10.rs
+++ src/solutions/year_2025/day10.rs
@@ -2,6 +2,7 @@ use itertools::Itertools;
 use std::{
     collections::{BTreeMap, HashSet},
     str::FromStr,
+    time::Instant,
 };
 
 use crate::AocSolution;
@@ -123,28 +124,31 @@ fn find_fewest_buttons_joltage(
     combos: &ButtonCombos,
     cache: &mut BTreeMap<Joltage, u64>,
 ) -> u64 {
+    // eprintln!("DEBUG: search joltage {:?}", joltage);
     if joltage.iter().map(|&j| j as u64).sum::<u64>() == 0 {
+        // eprintln!("DEBUG: return 0");
         return 0;
     }
     if let Some(&cached_value) = cache.get(joltage) {
+        // eprintln!("DEBUG: return cached value {:?}", cached_value);
         return cached_value;
     }
     let parity: Vec<bool> = joltage.iter().map(|&x| x % 2 == 0).collect();
-    // find combos that work with our parity
-    if !combos.contains_key(&parity) {
-        return LARGE;
-    }
+    // dbg!(&parity);
 
     // special case for all even parity
-    let special_val: u64 = if parity.iter().all(|x| !x) {
+    let special_val: u64 = if parity.iter().all(|x| *x) {
+        // eprintln!("DEBUG: Parity all even");
         combos
             .values()
             .map(|pmap| {
                 pmap.iter()
                     .map(|(result, presses)| {
                         if joltage.iter().zip(result.iter()).any(|(&j, &r)| j < 2 * r) {
+                            // eprintln!("DEBUG: even search: skip result {:?}", result);
                             LARGE
                         } else {
+                            // eprintln!("DEBUG: even search: trying result {:?}", result);
                             let new_joltage: Joltage = joltage
                                 .iter()
                                 .zip(result.iter())
@@ -160,28 +164,36 @@ fn find_fewest_buttons_joltage(
             .min()
             .unwrap()
     } else {
+        // eprintln!("DEBUG: Parity not all even");
         LARGE
     };
+    // dbg!(special_val);
 
     // Also search single application of parity
-    let value = combos[&parity]
-        .iter()
-        .map(|(result, presses)| {
-            if joltage.iter().zip(result.iter()).any(|(j, r)| j < r) {
-                LARGE
-            } else {
-                let new_joltage: Joltage = joltage
-                    .iter()
-                    .zip(result.iter())
-                    .map(|(j, r)| j - r)
-                    .collect();
-                let presses = *presses as u64;
-                find_fewest_buttons_joltage(&new_joltage, combos, cache) + presses
-            }
-        })
-        .min()
-        .unwrap()
-        .min(special_val);
+    let value = if !combos.contains_key(&parity) {
+        // eprintln!("DEBUG: Parity miss");
+        LARGE
+    } else {
+        combos[&parity]
+            .iter()
+            .map(|(result, presses)| {
+                if joltage.iter().zip(result.iter()).any(|(j, r)| j < r) {
+                    LARGE
+                } else {
+                    let new_joltage: Joltage = joltage
+                        .iter()
+                        .zip(result.iter())
+                        .map(|(j, r)| j - r)
+                        .collect();
+                    let presses = *presses as u64;
+                    find_fewest_buttons_joltage(&new_joltage, combos, cache) + presses
+                }
+            })
+            .min()
+            .unwrap()
+            .min(special_val)
+    };
+    // dbg!(value);
     cache.insert(joltage.clone(), value);
     value
 }
@@ -198,12 +210,23 @@ impl AocSolution for Day10 {
     }
 
     fn part2(&self, input: &str) -> String {
+        let start = Instant::now();
         parse(input)
             .iter()
+            .inspect(|m| {
+                eprintln!(
+                    "t:{} Working on machine {:?}",
+                    start.elapsed().as_secs_f32(),
+                    m
+                )
+            })
             .map(|m| {
                 let combos = map_button_combinations(&m.buttons);
                 let mut cache = BTreeMap::new();
-                find_fewest_buttons_joltage(&m.joltage, &combos, &mut cache)
+                let result = find_fewest_buttons_joltage(&m.joltage, &combos, &mut cache);
+                // dbg!(&combos);
+                // dbg!(&cache);
+                result
             })
             .sum::<u64>()
             .to_string()
@@ -254,8 +277,16 @@ mod tests {
     }
 
     #[test]
+    fn test_part2_example_1() {
+        assert_eq!(
+            dbg!(Day10.part2("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}")),
+            "10"
+        );
+    }
+
+    #[test]
     fn test_part2_example() {
-        assert_eq!(Day10.part2(EXAMPLE), "33");
+        assert_eq!(dbg!(Day10.part2(EXAMPLE)), "33");
     }
 
     #[test]
